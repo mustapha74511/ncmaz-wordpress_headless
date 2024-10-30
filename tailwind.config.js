21e73cb1580faec21474c740222a9c9df85f734a
74511 <bidolaster@gmail.com>
2024-10-30 15:25:53 +0100

    Update styles and files

--- tailwind.config.js
+++ tailwind.config.js
@@ -49,6 +49,7 @@ module.exports = {
           800: customColors("--c-primary-800"),
           900: customColors("--c-primary-900"),
         },
+        logoc: '#1E3A8A',
         secondary: {
           50: customColors("--c-secondary-50"),
           100: customColors("--c-secondary-100"),
